26bf4192e86c7aa8bfcb1a9ac85a7ef8a1dcfeef
Vincent Francais <vincent.francais@yahoo.fr>
2015-11-07 14:40:04 +0100

    Adding Main component

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,6 +2,5 @@ var gulp   = require('gulp');
 var config = require('../config.js');
 
 gulp.task('watch', ['browserSync'], function() {
-  gulp.watch(config.browserify.entries, ['browserify']);
   gulp.watch(config.markup.src, ['markup']);
 });
---
+++ src/app/components/Main.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+
+/* custom modules */
+
+
+/* styles modules */
+
+
+var Main = React.createClass({
+  render: function(){
+    return (
+      <div>
+        Hello world from Main
+      </div>
+    );
+  }
+});
+
+module.exports = Main;
